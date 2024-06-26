a2afdbe2ac6c213d0cabb3901f04cb6c1b5804ac
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2024-06-26 18:00:06 -0400

    sentiment analysis completed.

---
+++ backend/backend-env/config/analyze.py
@@ -0,0 +1,30 @@
+from firebase import get_all
+import nltk
+from nltk.tokenize import word_tokenize
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Download the necessary NLTK data
+nltk.download('punkt')
+
+# Fetch all documents
+res = get_all(limit=10)
+# print(res)  # Print to inspect the structure
+
+# Process tweets
+tweets = [entry['Tweet'] for entry in res]
+
+def tokenize_tweets_nltk(tweets):
+    tokenized_tweets = [word_tokenize(tweet) for tweet in tweets]
+    return tokenized_tweets
+
+def sentiment_analysis_vader(tokenized):
+    analyzer = SentimentIntensityAnalyzer()
+    sentiments = [analyzer.polarity_scores(tweet) for tweet in tweets]
+    return sentiments
+
+tokenized = tokenize_tweets_nltk(tweets)
+sentiment = sentiment_analysis_vader(tokenized)
+
+compoundSent = [entry['compound'] for entry in sentiment]
+
+print(compoundSent)
