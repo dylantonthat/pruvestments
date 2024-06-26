9abb284b3ed774084166fb2b7ababa666603babd
dylantonthat <dylantonthat107@gmail.com>
2024-06-26 16:22:24 -0400

    installed recharts for visualization, developing  SPA components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from './components/landing';
+import Sentiment_Visual from './components/sentiment_visual';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container mx-auto p-4">
+          <div className="w-full md:w-1/2 mx-auto">
+            <Landing />
+            <Sentiment_Visual />
+          </div>
+        </div>
     </div>
   );
 }
---
+++ frontend/src/components/landing.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+          <h2 class="text-xs text-blue-500 tracking-widest font-medium title-font mb-1">
+            Beyond the 404 Presents
+          </h2>
+          <h1 class="sm:text-4xl text-3xl font-medium title-font mb-4 text-gray-900">
+            Pruvestments
+          </h1>
+          <p class="lg:text-xl text-lg lg:w-2/3 mx-auto leading-relaxed">
+            AI-Powered Social Sentiment Stock Analysis
+          </p>
+        </div>
+        <div class="flex flex-wrap">
+          <div class="xl:w-1/3 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+              The Catalyzer
+            </h2>
+            <p class="leading-relaxed text-base mb-4">
+              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+              hexagon disrupt edison bulbche.
+            </p>
+            <a class="text-blue-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+          <div class="xl:w-1/3 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+              Neptune
+            </h2>
+            <p class="leading-relaxed text-base mb-4">
+              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+              hexagon disrupt edison bulbche.
+            </p>
+            <a class="text-blue-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+          <div class="xl:w-1/3 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+              Melanchole
+            </h2>
+            <p class="leading-relaxed text-base mb-4">
+              Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+              hexagon disrupt edison bulbche.
+            </p>
+            <a class="text-blue-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Landing;
