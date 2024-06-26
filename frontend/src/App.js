3cca046f0d9f510712f44ccaa2ee9113b413e750
carlylhubert <91027079+carlylhubert@users.noreply.github.com>
2024-06-26 17:18:23 -0400

    Update App.js
    side bar tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Landing from './components/landing';
 import Sentiment_Visual from './components/sentiment_visual';
+import Sidebar from './components/side_menu';
+
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
           <div className="w-full md:w-1/2 mx-auto">
             <Landing />
             <Sentiment_Visual />
-            <SideMenu/>
+            <Sidebar/>
           </div>
         </div>
     </div>
