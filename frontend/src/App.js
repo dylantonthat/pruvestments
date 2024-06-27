f7e5fad2e9afd748afbf96222456b8cf98bf8042
Fabiola Villatoro <fabiolamariavillatoro@gmail.com>
2024-06-27 08:15:40 -0400

    Uncommented footer, tackling graph issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Landing from './components/landing';
 import NavBar from './components/navbar';
 import SentimentVisual from './components/sentimentVisual';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -13,11 +14,11 @@ function App() {
             <SentimentVisual />
           </div>
         </div>
-        {/*
+        {
         <div style={{paddingTop: '20px', width: '100%', position: 'relative', zIndex: '1' }}>
             <Footer />
         </div>
-        */}
+        }
     </div>
   );
 }
